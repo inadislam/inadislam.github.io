62834497dd1c36b3200ea46d75949c40b048726b
inadislam <inadislam@gmail.com>
2022-04-24 15:51:29 +0600

    :sparkles: mdl added

---
+++ mlwbd-dl/assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    let url = window.location.href
+    $('li.nav-item a, .footer-menu li a').each(function () {
+        if (this.href === url) {
+            $(this).closest('li.nav-item a, .footer-menu li a').addClass('active')
+        }
+    })
+    function isUrlValid(url) {
+        return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+    }
+    $(".dlinks").click(function (e) {
+        e.preventDefault();
+        var murl = $(".main-section form").find('input[name="url"]').val();
+        if (murl == "") {
+            $(".download-links").html("<span class='text-danger'>Movie link not found.Fields must not be Empty!!</span>");
+        } else {
+            if (isUrlValid(murl)) {
+                $(".download-links").html("<span class='text-success'>Working...</span>");
+                $.ajax({
+                    type: "GET",
+                    url: `http://localhost:8080/dlink?murl=${murl}`,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        $(".download-links").html(response);
+                    }
+                });
+            } else {
+                $(".download-links").html("<span class='text-danger'>Link is not Valid. Please check your url again</span>");
+            }
+        }
+    });
+});
